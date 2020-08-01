220d18bec3660f749b102dcb889961d9e5c5d9fd
drzhang <654014730@qq.com>
2020-08-01 22:30:38 +0800

    测试

--- test/main.go
+++ test/main.go
@@ -1,28 +1,26 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
 	"os/exec"
 )
 
 func main() {
-	//cmd := exec.Command("/bin/sh", "git.sh", "git")
-	//err := cmd.Run()
-	//if err != nil {
-	//	panic(err)
-	//}
+	cmd := exec.Command("/bin/sh", "git.sh", "测试")
+	err := cmd.Run()
+	if err != nil {
+		panic(err)
+	}
 
-	cmd0 := exec.Command("/bin/bash", "-c","git.sh 'commit'")
-	var outputBuf1 bytes.Buffer
-	cmd0.Stdout = &outputBuf1
-	if err := cmd0.Start(); err != nil {
-		fmt.Printf("Error: The first command can not be startup %s\n", err)
-		return
-	}
-	if err := cmd0.Wait(); err != nil {
-		fmt.Printf("Error: Couldn't wait for the second command: %s\n", err)
-		return
-	}
-	fmt.Printf("%s\n", outputBuf1.Bytes())
+	//cmd0 := exec.Command("/bin/bash", "-c","git.sh 'commit'")
+	//var outputBuf1 bytes.Buffer
+	//cmd0.Stdout = &outputBuf1
+	//if err := cmd0.Start(); err != nil {
+	//	fmt.Printf("Error: The first command can not be startup %s\n", err)
+	//	return
+	//}
+	//if err := cmd0.Wait(); err != nil {
+	//	fmt.Printf("Error: Couldn't wait for the second command: %s\n", err)
+	//	return
+	//}
+	//fmt.Printf("%s\n", outputBuf1.Bytes())
 }
