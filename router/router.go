88f66b9cc07896cc2c17d1ebac91471efb215f29
drzhang <654014730@qq.com>
2020-08-02 16:00:03 +0800

    update

--- router/router.go
+++ router/router.go
@@ -3,18 +3,29 @@ package router
 import (
 	"blog/router/index"
 	"github.com/gin-gonic/gin"
+	"log"
 )
 
 func Run() {
 
 	r := gin.Default()
-	r.Static("static","static/")
-	r.StaticFile("static","static/")
+	r.Static("static", "static/")
+	r.StaticFile("static", "static/")
 	r.LoadHTMLGlob("static/view/*")
 
 	r.GET("/", index.Index)
 
-	r.POST()
+	r.POST("/login", func(c *gin.Context) {
+		var u = struct {
+			Username string `json:"username"`
+			Password string `json:"password"`
+		}{}
+		if err := c.ShouldBindJSON(&u); err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println("userinfo:", u)
+	})
 
-	r.Run(":8083")
+	r.Run(":8081")
 }
